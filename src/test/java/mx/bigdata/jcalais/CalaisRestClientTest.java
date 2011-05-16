27848aa8afb3044a8ba04a0f9caed3cc95567e4b
elmer.garduno <elmer.garduno@d18ce97d-3568-e82f-71c8-8e6a6608e092>
2011-05-16 16:41:33 +0000

    Renamed packages

--- src/test/java/mx/bigdata/jcalais/CalaisRestClientTest.java
+++ src/test/java/mx/bigdata/jcalais/CalaisRestClientTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2010 Elmer Garduno
+ *  Copyright 2010 BigData Mx
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  *  limitations under the License.
  *
  */
-package net.elmergarduno.jcalais;
+package mx.bigdata.jcalais;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
